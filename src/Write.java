bfd746b734740bcae2c8c2d010ab600e30fe9b2a
lenovo <lenovo@LAPTOP-70R5TJJL>
2021-11-14 12:36:09 +0530

    2 commit

--- src/Write.java
+++ src/Write.java
@@ -3,22 +3,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Write {
-	public static void main(String[] args) {
-		try {
-
-
-			FileWriter myObj = new FileWriter("test123.txt");
-			myObj.write("Files in Java might be tricky, but it is fun enough!123");
-
-			myObj.close();
-
-			System.out.println("Successfully wrote to the file.");
-		} 
-
-		catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
+	
+	public static void main (String args[]) throws IOException
+	{
+		File myobj=new File ("demo.txt");
+		
+		if(!myobj.exists())
+		{
+			myobj.createNewFile();
+			
 		}
+		
+		FileWriter mywrite= new FileWriter(myobj);
+		mywrite.write("This is testing");
+		mywrite.close();
+		System.out.println("Done");
 	}
 
 }
