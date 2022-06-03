2af8816ec55c45ed2ae277c687ee4312b5d98589
lenovo <lenovo@LAPTOP-70R5TJJL>
2022-06-03 23:53:32 +0530

    D array commit

---
+++ src/DuplicateElementsinArray.java
@@ -0,0 +1,23 @@
+
+public class DuplicateElementsinArray {
+	
+	public static void main(String[]args)
+	{
+	
+	int arr[]= {2, 5, 2, 6, 5,9,2,5};
+	
+	int len=arr.length;
+	
+	for(int i=0; i<len; i++)
+	{
+		for(int j=i+1; j<len; j++)
+		{
+			if(arr[i]==arr[j])
+			{
+				System.out.println(arr[i]);
+			}
+		}
+	}
+
+}
+}	
